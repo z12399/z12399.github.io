34c6d79e5751e3bc94a92ace8dea822befc26179
z12399 <z112233999@gmail.com>
2025-02-12 22:29:22 +0800

    Add files via upload

--- AssistData.js
+++ AssistData.js
@@ -1,55 +1,62 @@
 
 const assistData = {
-    '檸檬(0%)': {
+    '檸檬(0%增益,光)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.15,
         'critRate': 0,
         'critDamage': 0.4
     },
-    '檸檬(109%)': {
+    '檸檬(109%增益)': {
+        'baseAtk': 0,
+        'atkPercentage': 0,
+        'attributeDamage': 0,
+        'critRate': 0,
+        'critDamage': 0.836
+    },
+    '檸檬(109%增益,光)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.314,
         'critRate': 0,
         'critDamage': 0.836
     },
-    '桃子(0%)': {
+    '桃子(0%增益,火)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.15,
         'critRate': 0,
         'critDamage': 0
     },
-    '桃子(155%)': {
+    '桃子(155%增益,火)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.383,
         'critRate': 0,
         'critDamage': 0
     },
-    '辣椒碎片(0%)': {
+    '辣椒碎片(0%增益,火)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.20,
         'critRate': 0,
         'critDamage': 0
     },
-    '辣椒碎片(40%)': {
+    '辣椒碎片(40%增益,火)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.28,
         'critRate': 0,
         'critDamage': 0
     },
-    '搖滾(0%)': {
+    '搖滾(0%增益,水)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.30,
         'critRate': 0,
         'critDamage': 0
     },
-    '搖滾(155%)': {
+    '搖滾(155%增益,水)': {
         'baseAtk': 0,
         'atkPercentage': 0,
         'attributeDamage': 0.765,
